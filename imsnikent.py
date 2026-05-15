e24ceb821d7b913edeed997cab2b36ba32b8fcfc
kaccthony <113964745+kaccanthony@users.noreply.github.com>
2026-05-15 08:39:00 +0800

    HEEHEE

--- imsnikent.py
+++ imsnikent.py
@@ -1,7 +1,17 @@
+# inventory = {
+#     "Laptop": {"price": 50000, "stock": 5},
+#     "Mouse": {"price": 500, "stock": 20},
+#     "Keyboard": {"price": 1500, "stock": 10}
+# } for test
+
 def calc_total_inventory():
     total = 0
 
+    if not inventory:
+        print("Walay man laman bai")
+
     for product, details in inventory.items(): # subject to change
+
         value = details["price"] * details["stock"]
         total += value
 
@@ -10,6 +20,10 @@ def calc_total_inventory():
 def view_sales_history():
     print("\nSales History")
     print("-" * 30)
+
+    if not sales:
+        print("aray mo walang nabenta")
+        return
 
     for sale in sales_history: # subject to change
         print(
